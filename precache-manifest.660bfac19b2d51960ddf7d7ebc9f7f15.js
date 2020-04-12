60bbd2ee648fb53a344a779ed77df01305131e3c
Cyril Okidi <okidicyril@gmail.com>
2020-04-12 10:55:33 +0300

    Updates

---
+++ precache-manifest.660bfac19b2d51960ddf7d7ebc9f7f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4f4a32028dd48c0893560b0d37bd4d",
+    "url": "/covid-19-estimator-frontend/index.html"
+  },
+  {
+    "revision": "d595c9122c9c17698749",
+    "url": "/covid-19-estimator-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a3913ba0c2bbc2d1f32",
+    "url": "/covid-19-estimator-frontend/static/js/2.ada4e423.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-estimator-frontend/static/js/2.ada4e423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d595c9122c9c17698749",
+    "url": "/covid-19-estimator-frontend/static/js/main.59dfb6e4.chunk.js"
+  },
+  {
+    "revision": "f0b62999fbe44971fe4c",
+    "url": "/covid-19-estimator-frontend/static/js/runtime-main.09eec51d.js"
+  }
+]);
